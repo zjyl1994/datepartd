daec70edae4d0b3d7038c0a15c21e5ea9ba7038b
zjyl1994 <me@zjyl1994.com>
2020-10-25 11:07:27 +0800

    use crontab for auto work

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/robfig/cron/v3"
 	"github.com/zjyl1994/datepartd/mode"
 )
 
@@ -21,15 +21,36 @@ func main() {
 		log.Println(err.Error())
 		return
 	}
+	c := cron.New()
+	_, err = c.AddFunc("0 23 * * *", createPartitionJob)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	_, err = c.AddFunc("0 1 * * *", deletePartitionJob)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	c.Start()
+
+}
+
+func createPartitionJob() {
 	for _, v := range conf.Database {
 		if createFn, ok := createModes[v.Mode]; ok {
 			err := createFn(v)
-			fmt.Println(v.Name, "Create", err)
+			log.Println(v.Name, "Create", err)
 		}
+	}
+}
+
+func deletePartitionJob() {
+	for _, v := range conf.Database {
 		if v.PurgeDays > 0 {
 			if deleteFn, ok := deleteModes[v.Mode]; ok {
 				err := deleteFn(v)
-				fmt.Println(v.Name, "Delete", err)
+				log.Println(v.Name, "Delete", err)
 			}
 		}
 	}
